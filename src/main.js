cba691f0da55d98f20c6b702e6fe0eb37854108a
lutfii <lutfiakmal72@myra.com.my>
2025-11-05 00:47:17 +0800

    dev(our-project): development of ourproject sectoin

--- src/main.js
+++ src/main.js
@@ -5,3 +5,28 @@ import './javascripts/navbar.js';
 import './javascripts/slider.js';
 
 AOS.init();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector('.carousel')
+    const slides = document.querySelectorAll('.carousel a')
+    const nextBtn = document.querySelector('.next')
+    const prevBtn = document.querySelector('.prev')
+    let index = 0
+
+    function showSlide() {
+        carousel.style.transform = `translateX(-${index * 100}%)`
+    }
+
+    nextBtn.addEventListener('click', () => {
+        index = (index + 1) % slides.length
+        showSlide()
+    })
+
+    prevBtn.addEventListener('click', () => {
+        index = (index - 1 + slides.length) % slides.length
+        showSlide()
+    })
+
+});
+
